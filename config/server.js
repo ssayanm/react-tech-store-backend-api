b87875f4cead1d3c006b82ac6c2fe70c076f7eb3
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-08-23 08:11:33 +0530

    updating latest files

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '6d3673800eeda3cecb11c1783dea17bb'),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
 });
